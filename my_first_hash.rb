1e5e9e0d7ae8e193ec600a59e64977f7937ab618
Tabatha Roman <romantabatha17@gmail.com>
2020-09-12 22:43:13 +0000

    Automatically backed up by Learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -1,7 +1,5 @@
-def my_hash
-  # use the literal constructor to set the variable, my_hash, equal to a hash with key/value pairs of your choice.
-
-  
+def my_hash(hash)
+ your_hash{"name" => "Tabatha", "mother" => "Surjein"}
 end
 
 
